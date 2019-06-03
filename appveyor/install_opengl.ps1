ae2596a88a55961e429e8bd0bc36f6f169564290
Voltana <50107216+VoltanaDMG@users.noreply.github.com>
2019-06-03 14:43:35 +0200

    [~](Updated) `install_opengl.ps1`...
    ...This script installs GLFW and MesaOpenGL. Keep in mind, this script is only for CI who don't provide any binaries from builds. MesaOpenGL isn't really usefull for the `scg3` library.

--- appveyor/install_opengl.ps1
+++ appveyor/install_opengl.ps1
@@ -14,7 +14,14 @@ $GLFW_URL = "https://github.com/glfw/glfw/releases/download/"
 #
 #
 
-function DownloadOpenGL ($architecture) {
+$MESA_GL_URL = "https://github.com/vispy/demo-data/raw/master/mesa/"
+
+# Mesa DLLs found linked from:
+#     http://qt-project.org/wiki/Cross-compiling-Mesa-for-Windows
+# to:
+#     http://sourceforge.net/projects/msys2/files/REPOS/MINGW/x86_64/mingw-w64-x86_64-mesa-10.2.4-1-any.pkg.tar.xz/download
+
+function DownloadGLFW ($architecture) {
     [Net.ServicePointManager]::SecurityProtocol = 'Ssl3, Tls, Tls11, Tls12'
     $webclient = New-Object System.Net.WebClient
     # Download and retry up to 3 times in case of network transient errors.
@@ -74,9 +81,42 @@ function DownloadOpenGL ($architecture) {
     }
 }
 
+function DownloadMesaOpenGL ($architecture) {
+    [Net.ServicePointManager]::SecurityProtocol = 'Ssl3, Tls, Tls11, Tls12'
+    $webclient = New-Object System.Net.WebClient
+    # Download and retry up to 3 times in case of network transient errors.
+    $url = $MESA_GL_URL + "opengl32_mingw_" + $architecture + ".dll"
+    if ($architecture -eq "32") {
+        $filepath = "C:\Windows\SysWOW64\opengl32.dll"
+    } else {
+        $filepath = "C:\Windows\system32\opengl32.dll"
+    }
+    takeown /F $filepath /A
+    icacls $filepath /grant "${env:ComputerName}\${env:UserName}:F"
+    Remove-item -LiteralPath $filepath
+    Write-Host "Downloading" $url
+    $retry_attempts = 2
+    for($i=0; $i -lt $retry_attempts; $i++){
+        try {
+            $webclient.DownloadFile($url, $filepath)
+            break
+        }
+        Catch [Exception]{
+            Start-Sleep 1
+        }
+    }
+    if (Test-Path $filepath) {
+        Write-Host "File saved at" $filepath
+    } else {
+        # Retry once to get the error message if any at the last try
+        $webclient.DownloadFile($url, $filepath)
+    }
+}
+
 
 function main () {
-    DownloadOpenGL "32"
+    DownloadGLFW "64"
+    DownloadMesaOpenGL "64"
 }
 
 main
