b7741d0d6a777ce35849b939163eef1231f4467b
Voltana <50107216+VoltanaDMG@users.noreply.github.com>
2019-06-19 09:30:25 +0200

    [~](Updated) `setup_headless_display.sh`...
    ...Added update command because this script is also used by some normal users.

--- travis/setup_headless_display.sh
+++ travis/setup_headless_display.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
-sudo apt install libglu1-mesa-dev freeglut3-dev mesa-common-dev -y
+sudo apt update && sudo apt install libglu1-mesa-dev freeglut3-dev mesa-common-dev -y
 # Add glfw3 version 3.2.1
 version="3.2.1" && \
 wget "https://github.com/glfw/glfw/releases/download/${version}/glfw-${version}.zip" && \
