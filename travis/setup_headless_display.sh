44d3ee42befc3ddc09b33263f5d20cfcae0bf991
Voltana <50107216+VoltanaDMG@users.noreply.github.com>
2019-06-19 09:07:33 +0200

    [~](Updated) `setup_headless_display.sh`...
    ...created install script to install all required libraries to compile OpenGL programs

--- travis/setup_headless_display.sh
+++ travis/setup_headless_display.sh
@@ -1,1 +1,15 @@
-# 
+#!/bin/sh
+set -x
+sudo apt install libglu1-mesa-dev freeglut3-dev mesa-common-dev -y
+# Add glfw3 version 3.2.1
+version="3.2.1" && \
+wget "https://github.com/glfw/glfw/releases/download/${version}/glfw-${version}.zip" && \
+unzip glfw-${version}.zip && \
+cd glfw-${version} && \
+sudo apt-get install cmake xorg-dev libglu1-mesa-dev && \
+sudo cmake -G "Unix Makefiles" && \
+sudo make && \
+sudo make install && \
+cd .. && \
+sudo rm -rf glfw-${version}
+set +x
